227cb9b0ddb830af6e4f77408854ff9a2254f7e4
ecacarva <ecacarva2@gmail.com>
2012-12-19 08:38:07 -0300

    New data

---
+++ endocrown.R
@@ -0,0 +1,12 @@
+###################
+# endocrown.R
+###################
+
+#tabela 1 do artigo ou paragrafo com descricao da amostra
+
+# tabela 2 com anova comparando os 4 grupos. se nao for estatisticamente significativo para ai
+
+# se for significativo, 
+#compara grupos dois a dois com t-test e coloca no artigo
+
+# cria um boxplot comparando os 4 grupos
